4efc3f7b0a51621350ad29be1eb2d46a50ca3573
Adrian Pearman <adrianpearman12@gmail.com>
2018-10-04 18:02:54 -0400

    completion of version #1 of twitter clone application

--- client/client.js
+++ client/client.js
@@ -2,9 +2,12 @@ console.log('connected')
 
 const form = document.querySelector('form')
 const loadingElement = document.querySelector('.loading')
+const barksElement = document.querySelector('.barks')
 const API_URL = 'http://localhost:4000/barks'
 
-loadingElement.style.display = 'none'
+loadingElement.style.display = ''
+
+listAllBarks()
 
 form.addEventListener('submit', (e)=> {
   e.preventDefault()
@@ -20,6 +23,7 @@ form.addEventListener('submit', (e)=> {
   form.style.display = 'none'
   loadingElement.style.display = ''
 
+  // Handles the submission to the database
   fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(bark),
@@ -28,9 +32,42 @@ form.addEventListener('submit', (e)=> {
     }
   }).then((response) => { return response.json() })
     .then((createdBark) => { 
-      console.log(createdBark) 
+      // Clears the values inside of the form
       form.reset()
-      form.style.display = ''
+      // Delays the input form reappearing on the screen
+      setTimeout(() => {
+       form.style.display = ''        
+      }, 30000)
+      listAllBarks()
       loadingElement.style.display = 'none'
     })
 })
+
+// Retrieves all of the barks from the database
+function listAllBarks () {
+  barksElement.innerHTML = '' // Used to clear out the barks container
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(barks => {
+      barks.reverse()
+      barks.forEach(bark => {
+        const div = document.createElement('div')
+
+        const header = document.createElement('h1')
+        header.textContent = bark.name
+
+        const contentTag = document.createElement('p')
+        contentTag.textContent = bark.content
+
+        const dateTag = document.createElement('p')
+        dateTag.textContent = new Date(bark.created)
+
+        div.appendChild(header)
+        div.appendChild(contentTag)
+        div.appendChild(dateTag)
+
+        loadingElement.style.display = 'none'
+        barksElement.appendChild(div)
+      })
+    })
+}
--- server/server.js
+++ server/server.js
@@ -6,32 +6,53 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const monk = require('monk')
 const mongoose = require('mongoose')
+const Filter = require('bad-words')
+const rateLimit = require('express-rate-limit')
 
-// Using Monnk to connect to the database
-const db = monk('localhost/twitterBark')
+// Using Monk to connect to the database
+const db = monk(process.env.MONGO_URI || 'localhost/twitterBark')
 const barks = db.get('barks') // Grabs all of the saved barks
 
-const PORT = 4000
+// App Variables
+const PORT = process.env.PORT || 4000
+const filter = new Filter()
 
+
+// Middlewares
 app.use(cors())
 // app.use(express.json()) - Alternative to using bodyParser middleware
 app.use(bodyParser())
 
+isValidBark = (data) => {
+  return data.name && data.name.toString().trim() !== '' && data.content && data.content.toString().trim() !== '' 
+}
+
+// Routes
 app.get('/', (req, res) => {
   res.json({
     connected: 'true'
   })
 })
 
-isValidBark = (data) => {
-  return data.name && data.name.toString().trim() !== '' && data.content && data.content.toString().trim() !== '' 
-}
+app.get('/barks', (erq, res) => {
+  barks.find()
+    .then((barks) => {
+      res.json(barks)
+    })
+})
+
+app.use(
+  rateLimit({
+    windowMS: 30 * 1000, // 30 seconds
+    max: 100, // Limits each IP to 1 requests per windowMs
+  })
+)
 
 app.post('/barks', (req, res) => {
   if (isValidBark(req.body)) {
     const bark = {
-      name: req.body.name.toString(),
-      content: req.body.content.toString(),
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
       created: new Date()
     }
 
@@ -52,3 +73,4 @@ app.post('/barks', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`)
 })
+
